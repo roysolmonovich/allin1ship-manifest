485831375455dd4eb533e9722232b918144dc521
roysolmonovich <roysolmonovich@icloud.com>
2021-03-06 13:52:43 -0500

    removed redundant crap

--- hashes/services/service_hash.py
+++
@@ -1,9 +0,0 @@
-import csv
-from pathlib import Path
-
-
-base_path = Path(__file__).parent
-file_path = (base_path / "dhl_service_hash.csv").resolve()
-with open(file_path, mode='r') as infile:
-    reader = csv.reader(infile)
-    dhl_services = {rows[0]: rows[1] for rows in reader}
