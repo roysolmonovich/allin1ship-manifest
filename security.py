22fcb6a757432a7c5d8803c73da5ab8f1d2b74df
roysolmonovich <roysolmonovich@icloud.com>
2021-03-06 17:40:22 -0500

    dep

---
+++ security.py
@@ -0,0 +1,18 @@
+from models.user import UserModel
+from passlib.hash import pbkdf2_sha256
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if not user:
+        print(f'User {username} not found.')
+        return
+    if not pbkdf2_sha256.verify(password, user.hashed_pw):
+        print('Wrong password entered.')
+        return
+    return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_user_id(user_id)
+# user1 = User.find_by_username('rsolmonovich')
+# print(user1.password, len(user1.password))
