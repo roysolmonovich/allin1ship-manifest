136e098db9cb2f5e6db63448e2c370ed8d982ba5
roysolmonovich <roysolmonovich@icloud.com>
2021-03-31 16:41:18 -0400

    qb

---
+++ config.py
@@ -0,0 +1,24 @@
+DEBUG = False
+SQLALCHEMY_ECHO = False
+
+# OAuth2 credentials
+# Mendy's CLIENT_ID = 'ABa2d5HlIYnUi56csz2G7aqB6uwWu7i6bmDNv5UjapkGeKGpkL'
+# Mendy's CLIENT_SECRET = 'uryjXlsBCcy3I3lZwz64SWUziZK05NHLK5OyHo8k'
+# My CLIENT_ID = 'ABwEUav1LdGphUfD7MtbJihfXCKRowSV90rCSUOPEsXsDLserv'
+# My CLIENT_SECRET = 'J6IRGJ8KlIu21BnKlTHknot2OZeFTYM3rwvLxB0i'
+# Mendy's CLIENT_ID_2 = 'AB0jCWwJIrVymQ5iWZDwiy2LUIuXiNvNpUDzOLgPsLkDaxDbsO'
+# Mendy's CLIENT_SECRET_2 = 'INzkNGb5TsLI1iqEghyZoLqKAeOHkzWNWYdQN1Te'
+# Mendy's CLIENT_ID_3 = 'ABw4SNtV7DAcHwhzEDUHYfZu5chENS0Ggb3e4NU8NS5X0gcVAm'
+# Mendy's CLIENT_SECRET_3 = 'vmSF1ggrPuAl8Le3LXEEertBkF1gFPe86DOSjKBt'
+CLIENT_ID= 'ABw4SNtV7DAcHwhzEDUHYfZu5chENS0Ggb3e4NU8NS5X0gcVAm'
+CLIENT_SECRET = 'vmSF1ggrPuAl8Le3LXEEertBkF1gFPe86DOSjKBt'
+REDIRECT_URI = 'http://localhost:5000/callback'
+AUTH_TYPE = 'OAuth2'
+
+# Choose environment; default is sandbox
+# ENVIRONMENT = 'Sandbox'
+ENVIRONMENT = 'Production'
+
+# Set to latest at the time of updating this app, can be be configured to any minor version
+API_MINORVERSION = '40'
+
