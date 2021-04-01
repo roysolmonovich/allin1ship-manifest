da325d1bbe9d89920f3056bed138436dda49df9e
roysolmonovich <roysolmonovich@icloud.com>
2021-04-01 10:05:04 -0400

    celery-redis

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from flask_restful import Api, Resource
 from blocklist import BLOCKLIST
 from flask_jwt_extended import JWTManager, jwt_required
 import os
+from celery import Celery
 # from werkzeug.utils import secure_filename
 # from flask_wtf import FlaskForm
 # import mysql.connector
@@ -81,11 +82,18 @@ app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.config['UPLOAD_FOLDER'] = 'api_uploads'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+if os.environ.get('REDIS_URL'):
+    app.config['REDIS_URL'] = os.environ.get('REDIS_URL')
+else:
+    from redis_cred import redis_url
+    app.config['REDIS_URL'] = redis_url
 app.secret_key = 'roy'
 api = Api(app)
 # jwt = JWT(app, authenticate, identity)
 jwt = JWTManager(app)
-
+app.config['CELERY_BROKER_URL'] = app.config['REDIS_URL']
+app.config['CELERY_RESULT_BACKEND'] = app.config['REDIS_URL']
+celery = Celery(app.name, backend='redis', broker=app.config['REDIS_URL'])
 
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blocklist(jwt_header, jwt_payload):
@@ -300,12 +308,63 @@ def manifest_lookup():
     return jsonify({'message': 'No name chosen'}), 400
 
 
-# @app.route('/manifest_iter/', methods=['POST'])
-# def manifest_iter():
-#     request_data = request.get_json()
-#
-#     response = jsonify({})
-#     response.headers.add('Access-Control-Allow-Origin', '*')
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({'message': 'In progress.'}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
 
 api.add_resource(CarrierItem, '/maptest')
 api.add_resource(User, '/user')
