819e3ea1170b20e9a723d276651ecce8e85f8263
roysolmonovich <roysolmonovich@icloud.com>
2021-04-01 15:29:14 -0400

    conf

--- config.py
+++ config.py
@@ -2,22 +2,27 @@ DEBUG = False
 SQLALCHEMY_ECHO = False
 
 # OAuth2 credentials
-# Mendy's CLIENT_ID = 'ABa2d5HlIYnUi56csz2G7aqB6uwWu7i6bmDNv5UjapkGeKGpkL'
+# Mendy's CLIENT_ID - sandbox development = 'ABa2d5HlIYnUi56csz2G7aqB6uwWu7i6bmDNv5UjapkGeKGpkL'
 # Mendy's CLIENT_SECRET = 'uryjXlsBCcy3I3lZwz64SWUziZK05NHLK5OyHo8k'
 # My CLIENT_ID = 'ABwEUav1LdGphUfD7MtbJihfXCKRowSV90rCSUOPEsXsDLserv'
 # My CLIENT_SECRET = 'J6IRGJ8KlIu21BnKlTHknot2OZeFTYM3rwvLxB0i'
-# Mendy's CLIENT_ID_2 = 'AB0jCWwJIrVymQ5iWZDwiy2LUIuXiNvNpUDzOLgPsLkDaxDbsO'
+# Mendy's CLIENT_ID_2 - sandbox production = 'AB0jCWwJIrVymQ5iWZDwiy2LUIuXiNvNpUDzOLgPsLkDaxDbsO'
 # Mendy's CLIENT_SECRET_2 = 'INzkNGb5TsLI1iqEghyZoLqKAeOHkzWNWYdQN1Te'
-# Mendy's CLIENT_ID_3 = 'ABw4SNtV7DAcHwhzEDUHYfZu5chENS0Ggb3e4NU8NS5X0gcVAm'
+# Mendy's CLIENT_ID_3 - test app production = 'ABw4SNtV7DAcHwhzEDUHYfZu5chENS0Ggb3e4NU8NS5X0gcVAm'
 # Mendy's CLIENT_SECRET_3 = 'vmSF1ggrPuAl8Le3LXEEertBkF1gFPe86DOSjKBt'
-CLIENT_ID= 'ABwEUav1LdGphUfD7MtbJihfXCKRowSV90rCSUOPEsXsDLserv'
-CLIENT_SECRET = 'J6IRGJ8KlIu21BnKlTHknot2OZeFTYM3rwvLxB0i'
+# Mendy's CLIENT_ID_4 - test app development = 'ABoKrcajTt7QIjR1GnyruzZLhspMMaRn5B22F5qzhu22DRTfaz'
+# Mendy's CLIENT_SECRET_4 = 'r9E3V6u717j265mscKnXmolgweK5MVEgGYKsrbWZ'
+# Mendy's CLIENT_ID_5 - test app production = 'ABw4SNtV7DAcHwhzEDUHYfZu5chENS0Ggb3e4NU8NS5X0gcVAm'
+# Mendy's CLIENT_SECRET_5 = 'vmSF1ggrPuAl8Le3LXEEertBkF1gFPe86DOSjKBt'
+
+CLIENT_ID= 'ABoKrcajTt7QIjR1GnyruzZLhspMMaRn5B22F5qzhu22DRTfaz'
+CLIENT_SECRET = 'r9E3V6u717j265mscKnXmolgweK5MVEgGYKsrbWZ'
 REDIRECT_URI = 'https://allin1ship-app.herokuapp.com/callback'
 AUTH_TYPE = 'OAuth2'
 
 # Choose environment; default is sandbox
-ENVIRONMENT = 'Sandbox'
-# ENVIRONMENT = 'Production'
+# ENVIRONMENT = 'Sandbox'
+ENVIRONMENT = 'Production'
 
 # Set to latest at the time of updating this app, can be be configured to any minor version
 API_MINORVERSION = '40'
